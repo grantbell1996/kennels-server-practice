7790b271221c1f82941cbc25cac3e4bbc4b7c481
Grant Bell <grantbell@Grants-Air.attlocal.net>
2023-02-17 10:32:03 -0600

    added PUT method to request handler and tested

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -3,22 +3,25 @@ ANIMALS = [
         "id": 1,
         "name": "Snickers",
         "species": "Dog",
-        "locationId": 1,
-        "customerId": 4
+        "location": 1,
+        "customerId": 4,
+        "status": "Admitted"
     },
     {
         "id": 2,
         "name": "Gypsy",
         "species": "Dog",
-        "locationId": 1,
-        "customerId": 2
+        "location": 1,
+        "customerId": 2,
+        "status": "Admitted"
     },
     {
         "id": 3,
         "name": "Blue",
         "species": "Cat",
-        "locationId": 2,
-        "customerId": 1
+        "location": 2,
+        "customerId": 1,
+        "status": "Admitted"
     }
 ]
 
@@ -71,3 +74,12 @@ def delete_animal(id):
     # If the animal was found, use pop(int) to remove it from list
     if animal_index >= 0:
         ANIMALS.pop(animal_index)
+        
+def update_animal(id, new_animal):
+    # Iterate the ANIMALS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the animal. Update the value.
+            ANIMALS[index] = new_animal
+            break        
